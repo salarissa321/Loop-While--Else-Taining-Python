6b332f804c5a1d95ca8201f9982abefa46d98fb4
salarissa321 <salarissa321@gmail.com>
2023-05-22 11:43:53 +0200

    Loop-While- Else-Taining-Python

---
+++ Loop-While- Else-Taining-Python.py
@@ -0,0 +1,36 @@
+
+
+
+
+
+#---------------------------------------
+#-------- Loop => While Training
+#---------------------------------------
+# While Condition_is_True
+#    Code Will Run Unitl Condition become False
+#------------------------------------------
+
+myList = ["Sa" , "La" , "Pa" , "Fa" , "Gm" , "Ra" , "Al" , "Sh"]
+
+# print(len(myList)) # List Length # 8
+
+a = 0
+
+while a < len(myList): # a < 8
+    print(myList[a]) # Sa , La , Pa , Fa , Gm , Ra , Al , Sh 
+
+    a += 1 # a = a + 1
+
+else :
+    print("All Friends Printed to Screen") # All Friends Printed to Screen
+    
+
+
+# print(myList[0]) # Sa
+# print(myList[1]) # La
+# print(myList[2]) # Pa
+# print(myList[3]) # Fa
+# print(myList[4]) # Gm
+# print(myList[5]) # Ra
+# print(myList[6]) # Al
+# print(myList[7]) # Sh
